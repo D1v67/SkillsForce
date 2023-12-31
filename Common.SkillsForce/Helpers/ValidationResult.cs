994b6f0d790e987911b1a400803e56dfca6fb753
divesh <divesh.nugessur@ceridian.com>
2024-01-01 00:50:09 +0400

    Refactored Server side vlaiadtion

---
+++ Common.SkillsForce/Helpers/ValidationResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.SkillsForce.Helpers
+{
+    public class ValidationResult
+    {
+        public bool IsSuccessful { get; set; }
+        public List<string> Errors { get; set; }
+    }
+}
